0d278df45434b49e7b58505bdc3b16f426ba165e
Thangk15 <113288293+Thangk15@users.noreply.github.com>
2024-01-09 20:54:39 +0700

    edit style Home.js

--- Frontend/src/Component/ShortPost.js
+++ Frontend/src/Component/ShortPost.js
@@ -26,16 +26,17 @@ const ShortPost = ({ data, navigation }) => {
     return (
         <View style={styles.container}>
             <TouchableOpacity onPress={() => handlePress()}>
-                <View style={{ height: 150, width: 150, alignItems: 'center', justifyContent: 'center', backgroundColor: '#E8EAED', borderRadius: 12 }}>
+                <View style={{ height: 150, alignItems: 'center', justifyContent: 'center', backgroundColor: '#E8EAED', borderRadius: 12 }}>
                     <Image source={{ uri: data.Url }} style={{ width: 140, height: 140, borderRadius: 9 }} />
                     {/* <Image source={{uri:data.img}}></Image> */}
                 </View>
-                <View style={{ alignItems: "baseline", width: '100%' }}>
+
+                <View style={{ alignItems: "flex-start", width: '100%' }}>
                     <Text style={{ padding: 5, fontWeight: 600, fontSize: 14 }} numberOfLines={1}>{data.TenMA}</Text>
                 </View>
                 <View style={{ flexDirection: 'row', justifyContent: "space-between", alignItems: 'center', width: '100%', paddingHorizontal: 5 }}>
-                    <Text style={{ fontSize: 12, fontWeight: 300 }}>{formattedAmount(data.GiaTien)}đ</Text>
-                    <Text style={{ fontSize: 12, fontWeight: 300 }}>{HanderSold()}</Text>
+                    <Text style={{ fontSize: 11}}>{formattedAmount(data.GiaTien)}đ</Text>
+                    <Text style={{ fontSize: 11}}>{HanderSold()}</Text>
                 </View>
             </TouchableOpacity>
         </View>
@@ -48,7 +49,7 @@ const styles = StyleSheet.create({
     container: {
         // flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
+        // alignItems: 'center',
         // justifyContent: 'center',
         width: 150,
         height: 210,
